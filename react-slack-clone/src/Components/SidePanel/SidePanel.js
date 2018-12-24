ff7195b34b89fafbe37d2a944694aed750e3c400
Maher Khan <maher_khan@outlook.com>
2018-12-24 06:30:19 +0000

    Added channel functionality (created channels collection in db, gave users ability to add channels and added channels to global state)

--- react-slack-clone/src/Components/SidePanel/SidePanel.js
+++ react-slack-clone/src/Components/SidePanel/SidePanel.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {Menu} from "semantic-ui-react";
 import UserPanel from "./UserPanel";
+import Channels from "./Channels";
 
 class SidePanel extends React.Component {
     render () {
+        const {currentUser} = this.props;
+        
         return (
             <Menu size="large" inverted fixed="left" vertical stlyle={{background: "#4c3c4c", fontSize: "1.2rem"}}>
-                <UserPanel />
+                <UserPanel currentUser={currentUser} />
+                <Channels currentUser={currentUser} />
             </Menu>
         );
     }
